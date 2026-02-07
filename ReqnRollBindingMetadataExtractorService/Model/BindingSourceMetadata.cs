df9156f35fd15db9eae27d5d8ab0c47fa278fbb7
Rotbarsch <rhamke@googlemail.com>
2026-02-07 20:23:39 +0100

    Using mono.cecil instead of Reflection for performance and preventing file locks. (#6)
    Co-authored-by: Rotbarsch <denied@denied.com>

--- ReqnRollBindingMetadataExtractorService/Model/BindingSourceMetadata.cs
+++ ReqnRollBindingMetadataExtractorService/Model/BindingSourceMetadata.cs
@@ -1,9 +1,10 @@
 ﻿namespace ReqnRollBindingMetadataExtractorService.Model;
 
-public record StepDefinitionSourceMetadata
+public record BindingSourceMetadata
 {
     public required string Assembly { get; set; }
     public required string ClassName { get; set; }
-    public required string ClassFullName { get; set; }
     public required string MethodName { get; set; }
+    public required string ClassFullName { get; set; }
+    public required string ClassDescription { get; set; }
 }
