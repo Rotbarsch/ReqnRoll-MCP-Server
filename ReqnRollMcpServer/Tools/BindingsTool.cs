2778f8e4e73320363cf21e54008afc54583d01bf
Rotbarsch <denied@denied.com>
2026-02-22 12:16:47 +0100

    Referenced APACHE license of consumed ModelContextProtocol NuGet and improved tool capability detection with further descriptions

--- ReqnRollMcpServer/Tools/BindingsTool.cs
+++ ReqnRollMcpServer/Tools/BindingsTool.cs
@@ -1,5 +1,4 @@
 ﻿using System.ComponentModel;
-using System.Diagnostics;
 using System.Text.Json;
 using ModelContextProtocol.Server;
 using ReqnRollBindingMetadataExtractorService.Services;
@@ -7,10 +6,14 @@ using ReqnRollBindingMetadataExtractorService.Services;
 namespace ReqnRollMcpServer.Tools;
 
 [McpServerToolType]
+[Description("Provides tools for cataloguing ReqnRoll bindings.")]
 public static class BindingsTool
 {
-    [McpServerTool, Description("Returns information about all ReqnRoll bindings available. Provide the current working directory as the 'currentWorkingDirectory' parameter to scan for assemblies.")]
-    public static string GetAvailableBindings(string currentWorkingDirectory)
+    [McpServerTool(Name="get_bindings",Title="Get Reqnroll bindings available in workspace.")]
+    [Description("Returns information about all ReqnRoll bindings available by inspecting all available assemblies.")]
+    public static string GetAvailableBindings(
+        [Description("Current working directory containing the assemblies to inspect (they don't need to be in root; they can be anywhere in the working directory).")]
+        string currentWorkingDirectory)
     {
         return JsonSerializer.Serialize(BindingMetadataManager.GetAll(currentWorkingDirectory));
     }
