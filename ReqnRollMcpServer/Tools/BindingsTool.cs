9c1a7dfe3e4906cb28b66752d9eef4786d21002a
Rotbarsch <denied@denied.com>
2026-03-07 16:08:50 +0100

    Updated MCP to 1.0.0; introduced DNX compatibility

--- ReqnRollMcpServer/Tools/BindingsTool.cs
+++ ReqnRollMcpServer/Tools/BindingsTool.cs
@@ -7,11 +7,11 @@ namespace ReqnRollMcpServer.Tools;
 
 [McpServerToolType]
 [Description("Provides tools for cataloguing ReqnRoll bindings.")]
-public static class BindingsTool
+public class BindingsTool
 {
     [McpServerTool(Name="get_bindings",Title="Get Reqnroll bindings available in workspace.")]
     [Description("Returns information about all ReqnRoll bindings available by inspecting all available assemblies.")]
-    public static string GetAvailableBindings(
+    public string GetAvailableBindings(
         [Description("Current working directory containing the assemblies to inspect (they don't need to be in root; they can be anywhere in the working directory).")]
         string currentWorkingDirectory)
     {
