244722430f8f60991cf152a76bf8d2d5f8baf8d3
Rotbarsch <denied@denied.com>
2026-03-29 21:26:43 +0200

    added better cli functionality to doc generator

---
+++ ReqnRollBindingDocumentationGenerator/DocumentationGeneratorCommand.cs
@@ -0,0 +1,31 @@
+﻿using ReqnRollBindingMetadataExtractorService.Model;
+using ReqnRollBindingMetadataExtractorService.Services;
+using Spectre.Console.Cli;
+
+namespace ReqnRollBindingDocumentationGenerator;
+
+public class DocumentationGeneratorCommand : Command<DocumentationGeneratorSettings>
+{
+    public override int Execute(CommandContext context, DocumentationGeneratorSettings settings, CancellationToken cancellationToken)
+    {
+        var metaData = new List<BindingMetadata>();
+
+        var inputs = settings.Input.Select(s => s.Split(";"))
+            .Select(x=>new BindingAssemblyInput
+            {
+                Dll = x.First(),
+                Xml = x.Skip(1).FirstOrDefault(),
+            });
+
+        foreach (var input in inputs)
+        {
+            using var metadataService = new BindingMetadataExtractorService(input.Dll, input.Xml);
+            metaData.AddRange(metadataService.LoadMetadata());
+        }
+
+        var markdown = MarkdownGenerator.GenerateMarkdown(metaData);
+
+        File.WriteAllText(settings.OutputFileName, markdown);
+        return 0;
+    }
+}
---
+++ ReqnRollBindingDocumentationGenerator/DocumentationGeneratorSettings.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using Spectre.Console.Cli;
+
+namespace ReqnRollBindingDocumentationGenerator;
+
+public class DocumentationGeneratorSettings : CommandSettings
+{
+    [CommandOption("-i|--input",isRequired:true)]
+    [Description("Pairs of DLL paths and their corresponding XML doc file. Multiple usages possible. Example: -i a.dll;a.xml -i b.dll;b.xml")]
+    public string[] Input { get; set; }
+
+    [CommandOption("-o|--output",isRequired:true)]
+    [Description("Output file name for the generated markdown documentation.")]
+    public string OutputFileName { get; set; }
+
+}
